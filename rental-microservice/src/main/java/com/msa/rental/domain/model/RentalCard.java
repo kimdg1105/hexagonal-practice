d4602bb7347ccc44a1f7706ab7e94c6ef561b870
kimdg1105 <kimss7334@naver.com>
2023-11-10 16:37:36 +0900

    feat: 대여 비지니스 로직 구현

--- rental-microservice/src/main/java/com/msa/rental/domain/model/RentalCard.java
+++ rental-microservice/src/main/java/com/msa/rental/domain/model/RentalCard.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +30,82 @@ public class RentalCard {
 
     public static RentalCard create(IDName member) {
         return new RentalCard(RentalCardNo.create(), member, RentStatus.RENT_AVAILABLE, LateFee.create(0));
+    }
+
+    public RentalCard rentItem(Item item) {
+        checkRentalAvailable();
+        checkRentalItemMaxCount();
+        RentalItem rentalItem = RentalItem.create(item, LocalDate.now());
+        addRentalItem(rentalItem);
+        return this;
+    }
+
+    public RentalCard returnItem(Item item, LocalDate returnDate) {
+        this.rentalItemList.stream()
+                .filter(rentalItem -> rentalItem.getItem().equals(item))
+                .findFirst()
+                .ifPresent(rentalItem -> {
+                    calculateLateFee(rentalItem, returnDate);
+                    ReturnItem returnItem = ReturnItem.create(rentalItem);
+                    addReturnItem(returnItem);
+                    removeRentalItem(rentalItem);
+                });
+        return this;
+    }
+
+    public RentalCard overdueItem(Item item) {
+        rentalItemList.stream()
+                .filter(rentalItem -> rentalItem.getItem().equals(item))
+                .findFirst()
+                .ifPresent(rentalItem -> {
+                    rentalItem.overdue();
+                    this.rentStatus = RentStatus.RENT_UNAVAILABLE;
+                });
+        return this;
+
+    }
+
+    public Integer makeRentalCardAvailable(int point) {
+        checkRentalItemListIsEmpty();
+        checkLateFeeIsSmallerThanPoint(point);
+
+        this.lateFee = this.lateFee.removePoint(point);
+        if (lateFee.getPoint() == 0) {
+            this.rentStatus = RentStatus.RENT_AVAILABLE;
+        }
+        return this.lateFee.getPoint();
+    }
+
+    private void checkLateFeeIsSmallerThanPoint(int point) {
+        if (this.lateFee.getPoint() > point) {
+            throw new IllegalArgumentException("포인트가 부족합니다.");
+        }
+    }
+
+    private void calculateLateFee(RentalItem rentalItem, LocalDate returnDate) {
+        if (rentalItem.isOverdue()) {
+            int point = Period.between(rentalItem.getOverdueDate(), returnDate).getDays() * 10;
+            this.lateFee.addPoint(point);
+        }
+
+    }
+
+    private void checkRentalItemMaxCount() {
+        if (this.rentalItemList.size() > 5) {
+            throw new IllegalStateException("대여 가능한 아이템의 수를 초과하였습니다.");
+        }
+    }
+
+    private void checkRentalItemListIsEmpty() {
+        if (!this.rentalItemList.isEmpty()) {
+            throw new IllegalStateException("반납되지 않은 아이템이 있습니다.");
+        }
+    }
+
+    private void checkRentalAvailable() {
+        if (this.rentStatus == RentStatus.RENT_UNAVAILABLE) {
+            throw new IllegalStateException("대여 불가능한 상태입니다.");
+        }
     }
 
     private void addRentalItem(RentalItem rentalItem) {
--- rental-microservice/src/main/java/com/msa/rental/domain/model/RentalItem.java
+++ rental-microservice/src/main/java/com/msa/rental/domain/model/RentalItem.java
@@ -1,27 +1,37 @@
 package com.msa.rental.domain.model;
 
 import com.msa.rental.domain.model.vo.Item;
+import lombok.Getter;
 
 import java.time.LocalDate;
 
+@Getter
 public class RentalItem {
     private Item item;
     private LocalDate rentalDate;
     private LocalDate overdueDate;
-    private boolean overdued;
+    private boolean overdue;
 
     public static RentalItem create(Item item, LocalDate localDate) {
         RentalItem rentalItem = new RentalItem();
         rentalItem.item = item;
         rentalItem.rentalDate = localDate;
         rentalItem.overdueDate = localDate.plusDays(7);
-        rentalItem.overdued = false;
+        rentalItem.overdue = false;
 
         return rentalItem;
     }
 
-    public void overdue() {
-        this.overdued = true;
+    public boolean isOverdue() {
+        boolean result = LocalDate.now().isAfter(this.overdueDate);
+        if (result) {
+            this.overdue();
+        }
+        return result;
+    }
+
+    protected void overdue() {
+        this.overdue = true;
     }
 
 
